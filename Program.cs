4a481b63403a03064fc67abe937cdfef51266c07
JacksonBruns <28brunsj@newbremenschools.org>
2026-01-08 19:15:29 +0000

    done

--- Program.cs
+++ Program.cs
@@ -1,34 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+Random dice = new Random();
+int roll = dice.Next(1,6);
 
-app.Urls.Add("http://localhost:5000");
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
-
-app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
+if(roll >3)
 {
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
+    Console.Write(">3");
 }
+ else if (roll%2 == 0)  {
+    Console.WriteLine("even");
+} else {
+    Console.WriteLine("Neither");
 
-public record Weather
-{
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
 }
+Console.WriteLine(roll);
+
+
